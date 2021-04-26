7bcbf8f008286eb9d20788541eada1ef0e2a9f8c
Leres1 <volodyakisilv@gmail.com>
2021-04-26 23:29:44 +0300

    in process

--- web/keydown.js
+++ web/keydown.js
@@ -1,8 +1,17 @@
+activeLetter = document.querySelector('letter').textContent;
+document.querySelector('.lng-' + activeLetter).style.color = 'rgb(255, 128, 0)';
+
 document.onkeydown = function(event){
+    activeLetter = document.querySelector('letter:not(.correct)');
+    if(event.key == activeLetter.textContent){
+        activeLetter.style.color = 'rgb(255, 128, 0)';
+        activeLetter.classList.add('correct');
+    }
+
     for(let key in langArr){
         if(event.key == langArr[key][window.location.hash.substr(1)]){
-            document.querySelector('.lng-' + key).style.backgroundColor = 'rgb(100, 100, 100)';
+            document.querySelector('.lng-' + key).style.color = 'rgb(255, 128, 0)';
         }
     }
-    document.querySelector('.textLine').innerHTML += event.key;
+    // document.querySelector('.textLine').innerHTML += event.key;
 }
