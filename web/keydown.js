fa2dce5173f09608f81a958b6a154d08b441b755
Leres1 <volodyakisilv@gmail.com>
2021-04-28 19:11:44 +0300

    in process

--- web/keydown.js
+++ web/keydown.js
@@ -1,5 +1,6 @@
 activeLetter = document.querySelector('letter:not(correct)');
 document.querySelector('.lng-' +  activeLetter.textContent).style.color = 'rgb(255, 128, 0)';
+// document.querySelector('.lng-space').style.backgroundColor = 'rgb(255, 128, 0)';
 
 document.onkeydown = function(event){
     activeLetter = document.querySelector('letter:not(.correct)');
@@ -10,9 +11,9 @@ document.onkeydown = function(event){
         }
         activeLetter = document.querySelector('letter:not(.correct)');
         if(event.key != ' '){
-            document.querySelector('.lng-' + event.key).style.color = 'rgb(0, 0, 0)';
+            document.querySelector('.lng-' + event.key).style.color = 'rgb(101,101,101)';
         }else{
-            document.querySelector('.lng-space').style.color = 'rgb(0, 0, 0)';
+            document.querySelector('.lng-space').style.backgroundColor = 'rgb(51, 51, 51)';
         }
     }else{
         activeLetter.classList = 'incorrect';
@@ -23,11 +24,11 @@ document.onkeydown = function(event){
 
     for(let key in langArr){
         if(activeLetter.textContent == langArr[key][window.location.hash.substr(1)]){
-            // if(activeLetter.textContent == '_'){
-            //     document.querySelector('.lng-space').style.color = 'rgb(255, 128, 0)';
-            // }
             document.querySelector('.lng-' + key).style.color = 'rgb(255, 128, 0)';
         }
+    }
+    if(activeLetter.textContent == '_'){
+        document.querySelector('.lng-space').style.backgroundColor = 'rgb(255, 128, 0)';
     }
     // document.querySelector('.textLine').innerHTML += event.key;
 }
